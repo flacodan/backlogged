46cfa50316175d67e22db900d9a9e90e27e85bb8
Dan Hill <danshill@gmail.com>
2024-01-11 18:40:53 -0700

    add db models

---
+++ models/models.js
@@ -0,0 +1,111 @@
+import Sequelize, { DataTypes, Model } from 'sequelize';
+
+const db = new Sequelize('postgresql:///backlogged', { 
+    define: { underscored: true }
+});
+
+export class User extends Model {}
+
+User.init(
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+        }
+    },
+    {
+        modelName: 'user',
+        sequelize: db,
+        timestamps: false
+    }
+)
+
+
+export class Intention extends Model {}
+
+Intention.init(
+    {
+        // user_id as foreign key from User
+        intention_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+        category: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        percent: {
+            type: DataTypes.INTEGER,
+        },
+        time_est: {
+            type: DataTypes.INTEGER,
+        },
+        due_date: {
+            type: DataTypes.DATE,
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        complete_date: {
+            type: DataTypes.DATE,
+        },
+        priority: {
+            type: DataTypes.INTEGER,
+        }
+    },
+    {
+        modelName: 'intention',
+        sequelize: db,
+        updatedAt: false
+    }
+)
+
+
+export class Preferences extends Model {}
+
+Preferences.init(
+    {
+        language: {
+            type: DataTypes.STRING(30),
+        },
+        theme: {
+            type: DataTypes.STRING(30),
+        },
+        default_sort: {
+            type: DataTypes.STRING(30),
+        }
+    },
+    {
+        modelName: 'preferences',
+        sequelize: db,
+        timestamps: false
+    }
+)
+
+//TABLE RELATIONSHIPS
+User.hasMany(Intention, { foreignKey: 'user_id' });
+Intention.belongsTo(User, { foreignKey: 'user_id' });
+
+User.hasOne(Preferences, { foreignKey: 'user_id' });
+Preferences.belongsTo(User, { foreignKey: 'user_id' });
+
+// await db.sync({ force: true });
+// await db.close();
