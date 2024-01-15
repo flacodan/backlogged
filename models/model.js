57160d1545225867c6ce9dec0b4e2bab738182d4
Dan Hill <danshill@gmail.com>
2024-01-14 22:48:55 -0700

    Added .env and a few other minor things

--- models/model.js
+++ models/model.js
@@ -1,118 +1,116 @@
-import { DataTypes, Model } from 'sequelize';
-import connectToDB from '../db.js';
+import { DataTypes, Model } from "sequelize";
+import connectToDB from "../db.js";
 
+const dbURI = REACT_APP_DATABASE_URL;
 
-export const db = await connectToDB('postgresql:///backlogged');
+export const db = await connectToDB(dbURI);
 
 export class User extends Model {}
 
 User.init(
-    {
-        user_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        username: {
-            type: DataTypes.STRING(60),
-            allowNull: false,
-        },
-        password: {
-            type: DataTypes.STRING(60),
-            allowNull: false,
-        }
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        modelName: 'user',
-        sequelize: db,
-        timestamps: false
-    }
-)
-
+    username: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+  },
+  {
+    modelName: "user",
+    sequelize: db,
+    timestamps: false,
+  }
+);
 
 export class Goal extends Model {}
 
 Goal.init(
-    {
-        // user_id as foreign key from User
+  {
+    // user_id as foreign key from User
 
-        goal_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        title: {
-            type: DataTypes.STRING(40),
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-        },
-        category: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-        },
-        percent: {
-            type: DataTypes.INTEGER,
-        },
-        time_est: {
-            type: DataTypes.INTEGER,
-        },
-        due_date: {
-            type: DataTypes.DATE,
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
-        complete_date: {
-            type: DataTypes.DATE,
-        },
-        priority: {
-            type: DataTypes.INTEGER,
-        }
+    goal_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        modelName: 'goal',
-        sequelize: db,
-        updatedAt: false
-    }
-)
-
+    title: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    category: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    percent: {
+      type: DataTypes.INTEGER,
+    },
+    time_est: {
+      type: DataTypes.INTEGER,
+    },
+    due_date: {
+      type: DataTypes.DATE,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    complete_date: {
+      type: DataTypes.DATE,
+    },
+    priority: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    modelName: "goal",
+    sequelize: db,
+    updatedAt: false,
+  }
+);
 
 export class Preferences extends Model {}
 
 Preferences.init(
-    {
-        // user_id as foreign key from User
+  {
+    // user_id as foreign key from User
 
-        language: {
-            type: DataTypes.STRING(30),
-        },
-        theme: {
-            type: DataTypes.STRING(30),
-        },
-        default_sort: {
-            type: DataTypes.STRING(30),
-        }
+    language: {
+      type: DataTypes.STRING(30),
     },
-    {
-        modelName: 'preferences',
-        sequelize: db,
-        timestamps: false
-    }
-)
+    theme: {
+      type: DataTypes.STRING(30),
+    },
+    default_sort: {
+      type: DataTypes.STRING(30),
+    },
+  },
+  {
+    modelName: "preferences",
+    sequelize: db,
+    timestamps: false,
+  }
+);
 
 //TABLE RELATIONSHIPS
-User.hasMany(Goal, { foreignKey: 'user_id' });
-Goal.belongsTo(User, { foreignKey: 'user_id' });
+User.hasMany(Goal, { foreignKey: "user_id" });
+Goal.belongsTo(User, { foreignKey: "user_id" });
 
-User.hasOne(Preferences, { foreignKey: 'user_id' });
-Preferences.belongsTo(User, { foreignKey: 'user_id' });
+User.hasOne(Preferences, { foreignKey: "user_id" });
+Preferences.belongsTo(User, { foreignKey: "user_id" });
 
 // await db.sync({ force: true });
 // await db.close();
-
 
 // in terminal: createdb backlogged
 // to test in node:
--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-import express from 'express';
-import ViteExpress from 'vite-express';
-import authCtrl from './src/controller.js';
+import express from "express";
+import ViteExpress from "vite-express";
+import authCtrl from "./src/controller.js";
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.REACT_APP_PORT || 3000;
 const app = express();
 
 app.use(express.json());
 ViteExpress.config({ printViteDevServerHost: true });
-
 
 const { getAuthData, addAuthData, updateAuthData, deleteAuthData } = authCtrl;
 // AUTH ENDPOINTS
@@ -17,13 +15,14 @@ app.post("/user", addAuthData);
 app.put("/user/:id", updateAuthData);
 app.delete("/user/:id", deleteAuthData);
 
-const { getClientData, addClientData, updateClientData, deleteClientData } = clientCtrl;
+const { getClientData, addClientData, updateClientData, deleteClientData } =
+  clientCtrl;
 // CLIENT ENDPOINTS
 app.get("/", getClientData);
 app.post("/user", addClientData);
 app.put("/user/:id", updateClientData);
 app.delete("/user/:id", deleteClientData);
 
-
-
-ViteExpress.listen(app, port, () => {console.log(`Server is listening http://localhost:${port}`)});
+ViteExpress.listen(app, port, () => {
+  console.log(`Server is listening http://localhost:${port}`);
+});
